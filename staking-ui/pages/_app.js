e906f7da8840c442ba68da9a0d8c994f7b88d148
Hanzhi <Hanzhi100@outlook.com>
2022-07-04 15:24:44 +0800

    wrap component with moralis provider

--- staking-ui/pages/_app.js
+++ staking-ui/pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider initializeOnMount={false}>
+      //initializeOnMount==false means that we are not connecting to a maralis
+      //server, we are just using its hook
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
