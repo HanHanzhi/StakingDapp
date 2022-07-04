302ddd20988f8a304fda5e500490b8b70b8cd2e1
Hanzhi <Hanzhi100@outlook.com>
2022-07-04 16:39:14 +0800

    add rewardTokenAbi.json

--- staking-ui/components/StakeDetails.js
+++ staking-ui/components/StakeDetails.js
@@ -9,5 +9,10 @@ import { useMoralis } from "react-moralis";
 export default function StakeDetails() {
   const { account } = useMoralis();
   console.log(account);
+
+  //reward token address
+  //reward token ABI: copy from defi-minimal file artifacts/contracts/rewardToken
+  //and added to a new file con constants/rewardTokenAbi.json
+
   return <div>Hi from stake details</div>;
 }
