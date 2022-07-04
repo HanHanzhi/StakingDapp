4fecf286096d4e4bff964e683b35affe48ff06c9
Hanzhi <Hanzhi100@outlook.com>
2022-07-04 15:36:20 +0800

    adding of connect wallet button

--- staking-ui/pages/index.js
+++ staking-ui/pages/index.js
@@ -1,5 +1,11 @@
 import styles from "../styles/Home.module.css";
+import Header from "../components/Header.js";
 
 export default function Home() {
-  return <div className={styles.container}>Hi</div>;
+  return (
+    <div className={styles.container}>
+      <Header />
+      Hi
+    </div>
+  );
 }
