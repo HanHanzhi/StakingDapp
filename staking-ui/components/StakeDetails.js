aa68ea66db87a86ac716fc94077c33a9920377ed
Hanzhi <Hanzhi100@outlook.com>
2022-07-04 16:26:58 +0800

    add basic stakeDetails component

---
+++ staking-ui/components/StakeDetails.js
@@ -0,0 +1,8 @@
+//what we wanna know?
+//num of tokens in our wallet
+//num of tokens staked
+//num of tokens earned
+
+export default function StakeDetails() {
+  return <div>Hi from stake details</div>;
+}
