c6c74cbff1fd9f735eb6764286d80cee5539de47
Hanzhi <Hanzhi100@outlook.com>
2022-07-07 17:44:41 +0800

    add getRtBalance api call

--- staking-ui/components/StakeDetails.js
+++ staking-ui/components/StakeDetails.js
@@ -4,10 +4,28 @@
 //num of tokens earned
 
 //first we need to figure out who is connected to the website using useMoralis
-import { useMoralis } from "react-moralis";
+//useWeb3Contract by Moralis allows us to call function on any contract
+import { useMoralis, useWeb3Contract } from "react-moralis";
+import {
+  stakingAddress,
+  stakingAbi,
+  rewardTokenAbi,
+  rewardTokenAddress,
+} from "../constants";
 
 export default function StakeDetails() {
   const { account } = useMoralis();
+
+  //this is how we call any function that we want using runContractFunction
+  const { runContractFunction: getRtBalance } = useWeb3Contract({
+    abi: rewardTokenAbi,
+    contractAddress: rewardTokenAddress,
+    functionName: "balanceOf",
+    params: {
+      account: account,
+    },
+  });
+
   console.log(account);
 
   //reward token address
