093e661bdfdcbf9f04a0fe76f97c85c8669a143f
Hanzhi <Hanzhi100@outlook.com>
2022-07-07 18:19:24 +0800

    add RtBalance display

--- staking-ui/components/StakeDetails.js
+++ staking-ui/components/StakeDetails.js
@@ -12,9 +12,14 @@ import {
   rewardTokenAbi,
   rewardTokenAddress,
 } from "../constants";
+//in react we use useState to keep track of the different state
+//it checks a value and renders the frontend everytime that value changes
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
 export default function StakeDetails() {
-  const { account } = useMoralis();
+  const { account, isWeb3Enabled } = useMoralis();
+  const { rtBalance, setRtBalance } = useState("0");
 
   //this is how we call any function that we want using runContractFunction
   const { runContractFunction: getRtBalance } = useWeb3Contract({
@@ -26,11 +31,34 @@ export default function StakeDetails() {
     },
   });
 
+  //to call getRtBlance we need to use a react function call useEffect
+  //useEffect(function,[dependency]), everytime any dependency changes we run the function
+  useEffect(() => {
+    //function: update the UI and get balances
+    if (isWeb3Enabled && account) {
+      updateUiValues();
+    }
+  }, [account, isWeb3Enabled]);
+
+  async function updateUiValues() {
+    const rtBalanceFromContract = (
+      await getRtBalance({ onError: (error) => console.log(error) })
+    ).toString;
+    const formattedRtBalanceFromContract = ethers.utils.formatUnites(
+      rtBalanceFromContract,
+      "ethers"
+    );
+    setRtBalance(formattedRtBalanceFromContract);
+  }
   console.log(account);
 
   //reward token address
   //reward token ABI: copy from defi-minimal file artifacts/contracts/rewardToken
   //and added to a new file con constants/rewardTokenAbi.json (same is done for staking)
 
-  return <div>Hi from stake details</div>;
+  return (
+    <div>
+      <div>RT Blance is: {rtBalance} </div>Hi from stake details
+    </div>
+  );
 }
