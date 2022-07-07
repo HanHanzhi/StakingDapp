bc2dceffa1fcfa8b2b87fdfe1fd3e062f6c47ccc
Hanzhi <Hanzhi100@outlook.com>
2022-07-07 17:45:10 +0800

    minor syntax fix

--- staking-ui/constants/index.js
+++ staking-ui/constants/index.js
@@ -2,7 +2,7 @@ const stakingAddress = "0x0165878a594ca255338adfa4d48449f69242eb8f";
 const rewardTokenAddress = "0x5fc8d32690cc91d4c39d9d3abcbd16989f875707";
 
 const stakingAbi = require("./stakingAbi.json");
-const rewardTokenAbi = require(".rewardTokenAbi.json");
+const rewardTokenAbi = require("./rewardTokenAbi.json");
 
 module.exports = {
   stakingAbi,
