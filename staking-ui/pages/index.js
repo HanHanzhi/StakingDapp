3f03696b55d694057db31aa7b0d9ea3e4bec0911
Hanzhi <Hanzhi100@outlook.com>
2022-07-09 17:58:15 +0800

    minor fix

--- staking-ui/pages/index.js
+++ staking-ui/pages/index.js
@@ -1,13 +1,14 @@
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header.js";
 import StakeDetails from "../components/StakeDetails";
+import StakeForm from "../components/StakeForm";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
       <StakeDetails />
-      Hi
+      <StakeForm />
     </div>
   );
 }
